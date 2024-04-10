3f341c06afdb4adccdd5ef5adfcca467e038c987
valegianco11 <127424052+valegianco11@users.noreply.github.com>
2024-04-10 13:04:02 +0200

    Update Professore.java

--- Schedulatore/src/Schedulatore/Professore.java
+++ Schedulatore/src/Schedulatore/Professore.java
@@ -1,5 +1,48 @@
 package Schedulatore;
 
 public class Professore {
+        String nome;
+        String cognome;
+        int nMaterie;
+        int nOre;
+        int[] scansioneOraria;
 
+        Professore(String nome, String cognome, int nMaterie, int nOre, int[] scansioneOraria){
+            this.nome=nome;
+            this.cognome=cognome;
+            this.nMaterie=nMaterie;
+            this.nOre=nOre;
+            this.scansioneOraria=scansioneOraria;
+        }
+        public String getNome() {
+            return nome;
+        }
+
+        public void setNome(String nome) {
+            this.nome = nome;
+        }
+
+        public String getCognome() {
+            return cognome;
+        }
+
+        public void setCognome(String cognome) {
+            this.cognome = cognome;
+        }
+
+        public int getnMaterie() {
+            return nMaterie;
+        }
+
+        public void setnMaterie(int nMaterie) {
+            this.nMaterie = nMaterie;
+        }
+
+        public int getnOre() {
+            return nOre;
+        }
+
+        public void setnOre(int nOre) {
+            this.nOre = nOre;
+        }
 }
